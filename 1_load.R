e0c68c5f9eb54ab716a7db41c83f3fefb677fcd8
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-06-05 13:37:47 +0100

    Limma loading of data

--- 1_load.R
+++ 1_load.R
@@ -5,3 +5,6 @@ library(knitr)
 library(limma)
 
 # need to load the raw microarray files and the probes with their blast scores
+targets <- readTargets("Data/targets.txt")
+
+raw_data <- read.maimages(targets, source= "agilent", green.only=TRUE)
