0ff1ecfe698fa228013fc1afecdc7bd3d731a55a
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-06-10 15:17:21 +0100

    Limma matrices for comparing circadian and tidal

--- 4_do.R
+++ 4_do.R
@@ -1,1 +1,14 @@
 # perform analysis here generate graphs and tables and so on
+
+# circadian time
+# design matrix
+circa <- factor(targets$Time)
+circa_design <- model.matrix(~circa)
+
+# contrast matrix
+circa_contrast <- makeContrasts("day-night", levels=circa_design)
+
+# make design matrix for tidal time
+tidal <- factor(targets$Tide)
+tidal_design <- model.matrix(~tidal)
+
