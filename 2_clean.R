544439417bd77f8ddde07f0c373d9e4e3bb00239
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-06-10 12:16:52 +0100

    Added comments
    Added in a knitr flag to make it ignore the graphs I
    drew for checking the normalisation

--- 2_clean.R
+++ 2_clean.R
@@ -7,7 +7,8 @@ dat <- backgroundCorrect(raw_data, method = "normexp", offset=16)
 
 dat$E <- normalizeBetweenArrays(dat$E, method="quantile")
 
+## @knitr
+# exploratory graphs for eyeballing normalisation 
 boxplot(log(as.matrix(dat)),las=2,ylab="Log2(Intensity)")
-
 mva.pairs(as.matrix(raw_data)[,c(1:10)])
 mva.pairs(as.matrix(dat))
